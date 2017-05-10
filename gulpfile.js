5b1174c143b206edeae805626a87fb2e78415790
Aldo Sorbara <aldo.sorbara@mercadolibre.com>
2017-05-09 23:41:20 -0300

    adding gulp task to replace accents and gzip bundles

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,19 @@
 var gulp = require('gulp'),
+    $ = require('gulp-load-plugins')(),
+    replaceName = require('gulp-replace-name'),
     minifyHtml = require("gulp-minify-html"),
     concat = require('gulp-concat'),
     rename = require("gulp-rename"),
     uglify = require('gulp-uglify'),
     cleanCSS = require('gulp-clean-css'),
     autoprefixer = require('gulp-autoprefixer'),
-    imagemin = require('gulp-imagemin');
-    sort = require('gulp-sort');
+    tap = require('gulp-tap'),
+    imagemin = require('gulp-imagemin'),
+    removeDiacritics = require('diacritics').remove,
+    sort = require('gulp-sort'),
+    removeAccents= require('remove-accents'),
+    sanitize = require("sanitize-filename"),
+    gzip   = require('gulp-gzip');
 
 var jsFiles = 'js/*.js',
     jQuery = 'js/jquery.js',
@@ -18,12 +25,14 @@ var jsFiles = 'js/*.js',
     imagesFiles = 'images/*.+(png|jpg|jpeg|gif|svg)',
     imagesDest = 'dist/images';
 
+
 gulp.task('scripts', function() {
   gulp.src([jQuery , jsFiles])
     .pipe(concat('scripts.js'))
     .pipe(gulp.dest(jsDest))
     .pipe(rename('scripts.min.js'))
     .pipe(uglify())
+    .pipe(gzip())
     .pipe(gulp.dest(jsDest));
 });
 
@@ -35,6 +44,7 @@ gulp.task('css', function() {
             cascade: false
         }))
       .pipe(concat('style.min.css'))
+      .pipe(gzip())
       .pipe(gulp.dest(cssDest));
 })
 
@@ -47,5 +57,26 @@ gulp.task('html', function() {
 gulp.task('images', function() {
   return gulp.src(imagesFiles)
     .pipe(imagemin())
-    .pipe(gulp.dest(imagesDest));
+    .pipe(replaceName(/\s/g, '_'))
+    .pipe(tap(function(file, t) {
+            var path = file.path.lastIndexOf('/');
+            var module = file.path.substring(path + 1);
+            var array = module.split('');
+            var newFileName = [];
+            array.forEach(function(entry) {
+                // console.log('entro aca', entry.charCodeAt(0), entry);
+                if (entry.charCodeAt(0) !== 769) {
+                  newFileName.push(entry);
+                }
+              // newFileName.push(removeAccents(removeDiacritics(entry)))
+            })
+            console.log(newFileName.join(""));
+            gulp.src(file.path)
+              .pipe(rename(newFileName.join("")))
+              .pipe(gulp.dest(imagesDest)); // ./dist/main/text/ciao/goodbye.md
+            // console.log(newFileName);
+            // var newFileName = removeAccents(removeDiacritics(module));
+            // console.log(removeAccents(removeDiacritics('holáááááá aldo.png')));
+            // console.log(removeAccents(removeDiacritics(newFileName)));
+          }))
 });
